4610d8213b5cc62259cd10191900ffdae7f10e28
Karine Mesropyan <karinemesropyan1984@gmail.com>
2022-01-26 10:02:41 +0400

    Xndir 10

--- script10.js
+++ script10.js
@@ -1,11 +1,16 @@
 const {log} = console;
 function toEndNumber (arr,num){
+  let count = 0;
   for (let i=0; i<arr.length; i++) {
-      if(arr[i] == num) {
-        arr.splice(i,1);
-        arr.push(num);
-    }
+      if(arr[i] === num) {
+       arr.splice(i,1);
+       i--;
+       count++; 
+      }
   } 
+  for(let i=0; i<count; i++){
+    arr.push(num);
+  }
   return arr;
 }
 log(toEndNumber([1, 2, 3, 1, 4], 1))
